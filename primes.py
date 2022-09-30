8088d60e086e9e944a9ba8e52c4ddceb7c155375
DvinHartoonian <dvin.hartoonian@gmail.com>
2022-09-30 13:54:47 +0100

    Add Prime Number Generator to generate certain amount of prime numbers

--- primes.py
+++ primes.py
@@ -3,4 +3,22 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+
+    for x in range(number_of_primes):
+        primeFound = False
+
+        while primeFound == False:
+            prime = True
+
+            for i in range(2, num):
+                if num % i == 0:
+                    prime = False
+
+            if prime:
+                list.append(num)
+                primeFound = True
+
+            num += 1
+
     return list
